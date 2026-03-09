f4ee4cb24e53fa73153dd4c4e5690712d9078c37
Adnan Sadik <adnansadik235@gmail.com>
2026-03-09 09:43:03 +0800

    naive attention update 1

--- sparse_attention.cu
+++ sparse_attention.cu
@@ -12,8 +12,20 @@ __global__ void naive_attention(
     int j = blockIdx.y*blockDim.y + threadIdx.y; // each columns handles the full head dimension for a single token. so we have to move along the column
     if( i>= seq_len || j>=head_dim) return;
 
-    S[i,:]= Q[i, :] @ K.T;
-    S= S/sqrt(head_dim);
-    A= softmax(S, dim=-1);
-    O = A @ V;
+    // S[i,:]= Q[i, :] @ K.T;
+    // S= S/sqrt(head_dim);
+    // A= softmax(S, dim=-1);
+    // O = A @ V;
+    float  S[1024]; // assuming seq_len <= 1024
+    // we need S[i,t]= dot (Q[i,:], K[t,:])
+    for (int t=0 ; t<=seq_len; t++)
+    {
+        float dot = 0.0f;
+        for(int s=0 ;s <= head_dim; s++)
+        {
+            
+            dot+= Q[i*head_dim+s]*K[t*head_dim+s];
+        }
+        S[t]= dot/sqrt((float)head_dim);
+    }
 }
